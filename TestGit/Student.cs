d30bb9517d79943fb76a3ee1967c7a320f7775cd
tanbo340 <120133783@qq.com>
2022-10-07 18:00:00 +0800

    增加student类

---
+++ TestGit/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGit
+{
+   public class Student
+    {
+        public int age { get; set; }
+        public string name { get; set; }
+    }
+}
